5313284e49bdf83f200758000ede9b8d9ba8a910
Armando Carlos Marino <armandomarino123@gmail.com>
2020-03-24 19:56:21 -0300

    La aplicación ahora maneja sesiones

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, escape
 
 app = Flask(__name__)
 
@@ -10,9 +10,21 @@ def index():
 if __name__ == "__main__":
 	app.run()
 
+app.secret_key = "12345"
+
 # Rutas de la página:
 
 # Ruta de inicio de sesion
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
+	if request.method == "POST":
+		session["usuario"] = request.form["usuario"]
+		return render_template('index.html')
+
 	return render_template("login.html")
+
+# Ruta para cerrar la sesión
+@app.route("/logout")
+def logout():
+	session.pop("usuario", None)
+	return render_template('index.html')
