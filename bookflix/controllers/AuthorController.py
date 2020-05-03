89e5a8d23ec65c1f384a400d3fa746ba21df4c01
fnahuelp <faustonahuelpinero@gmail.com>
2020-05-03 19:53:09 -0300

    Agregada la verificación de admin
    Hice un decorator para evitar repetir código, creo que se podría usar para el resto de los controllers

--- bookflix/controllers/AuthorController.py
+++ bookflix/controllers/AuthorController.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, session
+from flask import request, render_template, session, abort
 from flask import send_from_directory, url_for
 #from app.models.AuthModel import authmodel
 #from app.helpers.Utility import sendResponse
@@ -7,6 +7,14 @@ from models.libro import Libro
 from config import config
 
 class AuthorController():
+
+	def validate (func):
+		def val (*args, **kwargs):
+			if not "admin" in session:
+				abort (401)
+			else:
+				return func (*args, **kwargs)
+		return val
 
 	def __init__(self):
 		pass
@@ -19,9 +27,11 @@ class AuthorController():
 		autor = Author.id (autor_id)
 		return render_template('autores/show.html', autor=autor)
 
+	@validate
 	def create (self):#formulario
 		return render_template ('autores/agregar.html')
 
+	@validate
 	def create_author (self):
 		name = request.form.get('nombre', '')
 		if Author.existe (name):#chequear que no existe
@@ -30,9 +40,11 @@ class AuthorController():
 			Author.crear (name)
 			return self.index()
 
+	@validate
 	def edit (self, autor_id):#formulario
 		return render_template('autores/editar.html')
 
+	@validate
 	def edit_author (self, autor_id):
 		name = request.form.get('nombre', '')
 		Author.edit (autor_id, name)
