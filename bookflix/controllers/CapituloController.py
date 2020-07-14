ee5ff7b5b7e1426361b55b4ff7d35e97391ed99f
fnahuelp <faustonahuelpinero@gmail.com>
2020-07-14 12:24:48 -0300

    Update CapituloController.py

--- bookflix/controllers/CapituloController.py
+++ bookflix/controllers/CapituloController.py
@@ -65,7 +65,7 @@ class CapituloController(AbstractController):
 			errores.append("Fecha de publicacion incorrecta")
 		if "fechaVencimiento" in request.form and request.form["fechaVencimiento"]:
 			vdate = datetime.strptime(request.form["fechaVencimiento"], "%Y-%m-%d")
-			if vdate.date() > libro["fecha_vencimiento"]:
+			if vdate.date() > libro["fecha_vencimiento"] or vdate.date() < pdate.date():
 				errores.append("Fecha de vencimiento incorrecta")
 		else:
 			vdate = None
